9a52db07ce5c273a66762968f8df128b4d069616
davien21 <chidiebereekennia@gmail.com>
2022-02-26 02:51:40 +0100

    --feat: migrate app to Typescript

--- hooks/useShortAddress.tsx
+++ hooks/useShortAddress.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-const largeCutText = (text) =>
+const largeCutText = (text: string) =>
   `${text.substring(0, 8)}...${text.substring(text.length - 6)}`;
 
-const smallCutText = (text) =>
+const smallCutText = (text: string) =>
   `${text.substring(0, 3)}...${text.substring(text.length - 2)}`;
 
-function useShortAddress(address) {
+function useShortAddress(address: string) {
   if (!address) address = "********";
 
   largeCutText(address);
