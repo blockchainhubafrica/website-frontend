79c6a05e516befdd3d61198d2f7180faeb3dcc0f
davien21 <chidiebereekennia@gmail.com>
2022-03-02 02:26:52 +0100

    --feat: update base configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          200: "#00bdff",
+          400: "#0043ff",
+          600: "#11163b",
+          800: "#00081f",
+        },
+        gray: {
+          100: "#f8f8f8",
+          400: "#cfcfcf",
+        },
+        green: {
+          200: "#00cf8e",
+        },
+        pink: { 100: "#d0c4c4" },
+        orange: "#ff9900",
+      },
+      fontFamily: {
+        coolvetica: ["Coolvetica", "sans-serif"],
+        "open-sans": ["Open sans", "helvetica"],
+      },
+    },
     container: {
       center: true,
       padding: {
@@ -13,21 +35,6 @@ module.exports = {
         xl: "6rem",
         "2xl": "10rem",
       },
-    },
-    colors: {
-      "green-200": "#00cf8e",
-      "blue-200": "#00bdff",
-      "blue-400": "#0043ff",
-      "blue-600": "#11163b",
-      "blue-800": "#00081f",
-      "gray-100": "#f8f8f8",
-      "gray-400": "#cfcfcf",
-      "pink-100": "#d0c4c4",
-      orange: "#ff9900",
-    },
-    fontFamily: {
-      coolvetica: ["Coolvetica", "sans-serif"],
-      "open-sans": ["Open sans", "helvetica"],
     },
   },
   variants: {
@@ -42,7 +49,7 @@ module.exports = {
             maxWidth: "768px",
           },
           "@screen md": {
-            maxWidth: "992px",
+            maxWidth: "1024px",
           },
           "@screen lg": {
             maxWidth: "1536px",
