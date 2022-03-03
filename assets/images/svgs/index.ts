2eca9adc49bed0e86e0c4b30533ffc70c51229ca
Gospel Chinyereugo <gospelokpara@gmail.com>
2022-03-03 14:14:27 +0100

    --feat: added icons/images for testimonials

--- assets/images/svgs/index.ts
+++ assets/images/svgs/index.ts
@@ -1,5 +1,15 @@
 import LogoIcon from "./full-logo.svg";
 import CircleCloseIcon from "./circle-close-icon.svg";
 import TopRightArrowIcon from "./top-right-arrow.svg";
+import RightArrowIcon from "./right-arrow.svg";
+import TestimonialHeadshot1 from "./headshots/testimonial-1.svg";
+import TestimonialHeadshot2 from "./headshots/testimonial-2.svg";
 
-export { LogoIcon, TopRightArrowIcon, CircleCloseIcon };
+export {
+    LogoIcon,
+    TopRightArrowIcon,
+    CircleCloseIcon,
+    RightArrowIcon,
+    TestimonialHeadshot1,
+    TestimonialHeadshot2
+};
