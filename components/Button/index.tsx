4c8cf68fee72121c8baef042ba35055b85723935
davien21 <chidiebereekennia@gmail.com>
2022-03-07 22:08:11 +0100

    --feat: remove toast and add banner component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,41 +1,41 @@
 /** @format */
 
-import React from 'react';
-import { TopRightArrowIcon } from '../../assets/images';
-import styles from './button.module.css';
+import React from "react";
+import { TopRightArrowIcon } from "../../assets/images";
+import styles from "./button.module.css";
 
 interface Props {
-	children?: React.ReactNode;
-	onClick?: () => void;
-	className?: string | '';
-	type?: "button" | "submit" | "reset";
-	text?: string | '';
-	buttonType: string | '';
+  children?: React.ReactNode;
+  onClick?: () => void;
+  className?: string | "";
+  type?: "button" | "submit" | "reset";
+  text?: string | "";
+  buttonType: string | "";
 }
 
 const Button: React.FC<Props> = ({
-	children,
-	onClick,
-	className,
-	type,
-	text,
-	buttonType,
-	...rest
+  children,
+  onClick,
+  className,
+  type,
+  text,
+  buttonType,
+  ...rest
 }) => {
+  let containerClass = styles.container;
+  if (containerClass)
+    containerClass += ` ${className} flex items-center py-3 px-6`;
+  containerClass += ` ${styles[buttonType]}`;
+  console.log(containerClass);
 
-  	let containerClass = styles.container;
-	if (containerClass) containerClass += ` ${className} flex items-center py-3 px-6`;
-	containerClass += ` ${styles[buttonType]}`;
-	console.log(containerClass)
-
-	return (
-		<button type={type} onClick={onClick} className={containerClass} {...rest}>
-			<span className='mr-3'>{text || children}</span>
-			<span>
-				<TopRightArrowIcon/>
-			</span>
-		</button>
-	);
+  return (
+    <button type={type} onClick={onClick} className={containerClass} {...rest}>
+      <span className="mr-3">{text || children}</span>
+      <span>
+        <TopRightArrowIcon />
+      </span>
+    </button>
+  );
 };
 
-export default Button;
+export { Button };
