b773fb9f9f67b4fc75266f9dba8743009b27c7ee
Gospel Chinyereugo <gospelokpara@gmail.com>
2022-03-08 06:50:47 +0100

    --refactor: contact form fixes

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./contact.module.css";
+import { ContactForm } from "../../components";
 
 export default function ContactUsPage() {
   return (
-    <main className={styles["container"]}>
-      
+    <main className={`${styles["container"]} py-20 px-5 md:px-10 lg:px-20`}>
+      <div className="md:w-1/2 mb-20">
+        <h3 className={`${styles["spaced-heading"]} text-base`}>CONTACT US</h3>
+        <h3 className={`mb-4 text-3xl md:text-5xl font-coolvetica text-blue-600`}>
+          Let’s work together to build &amp; make the dream come true. 🙏🏽
+        </h3>
+      </div>
+
+      <div className="md:w-3/4">
+        <ContactForm />
+      </div>
     </main>
   );
 }
