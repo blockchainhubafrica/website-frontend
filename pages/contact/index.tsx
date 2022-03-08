e618be78c707b71cba1261627e42e378f93b7d49
Gospel Chinyereugo <gospelokpara@gmail.com>
2022-03-08 05:28:52 +0100

    --feat: base files for contact page

---
+++ pages/contact/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./contact.module.css";
+
+export default function ContactUsPage() {
+  return (
+    <main className={styles["container"]}>
+      
+    </main>
+  );
+}
