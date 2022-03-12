9ab0802067f1479d5e5c476a74bdd67a60cd5502
MayorErnest <eobumma@gmail.com>
2022-03-12 14:53:27 +0100

    feat: modify loader

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,6 @@
 export * from "./Header";
 export * from "./RegistrationModal";
 export * from "./Input";
-export * from "./Loader";
 export * from "./Marquee";
 export * from "./Products";
 export * from "./TestimonialSection";
@@ -9,3 +8,4 @@ export * from "./Events";
 export * from "./Banner";
 // export * from "./StorySection";
 export * from "./Button";
+export * from "./Loader";
