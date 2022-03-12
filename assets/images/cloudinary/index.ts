744ee29afcb762df6c211957759b2d2aa314d65e
davien21 <chidiebereekennia@gmail.com>
2022-03-12 23:30:15 +0100

    -feat: add cloudinary srcs file

---
+++ assets/images/cloudinary/index.ts
@@ -0,0 +1,46 @@
+const VideoBackdropSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647008207/Company-website/video-backdrop_jgkzzq.jpg";
+
+const XendLogoSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647059955/Company-website/xend-logo_m5gizx.png";
+
+const NearLogoSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647059964/Company-website/near-logo_qu3mvp.png";
+
+const AcexLogoSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647059959/Company-website/acex-logo_c7pgef.png";
+
+const CasperLogoSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647059957/Company-website/casper-logo_wv1t84.png";
+
+const VestHubLogoSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647059958/Company-website/vest-hub-logo_rsz3db.png";
+
+const CrevatalLogoSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647059955/Company-website/crevatal-logo_ufh5ne.png";
+
+const Shapes1Src =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647106527/Company-website/shapes-1_khubdw.png";
+
+const Shapes2Src =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647102398/Company-website/shapes-2_v5ktbs.png";
+
+const NduImageSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647006558/Company-website/productsDemoImg1_n7ctjc.png";
+
+const BeimaImageSrc =
+  "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647006557/Company-website/productsDemoImg2_w4sblz.png";
+
+export {
+  VideoBackdropSrc,
+  XendLogoSrc,
+  NearLogoSrc,
+  AcexLogoSrc,
+  CasperLogoSrc,
+  VestHubLogoSrc,
+  CrevatalLogoSrc,
+  Shapes1Src,
+  Shapes2Src,
+  NduImageSrc,
+  BeimaImageSrc,
+};
