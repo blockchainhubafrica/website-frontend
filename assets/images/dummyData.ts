6dbb42f9ac02bb980c8096e76620ff48813adf90
davien21 <chidiebereekennia@gmail.com>
2022-03-12 23:25:44 +0100

    -refactor: updates to support cloudinary image component

--- assets/images/dummyData.ts
+++ assets/images/dummyData.ts
@@ -1,20 +1,23 @@
-import { productsDemoImg1, productsDemoImg2 } from ".";
 import { eventTestImage } from "./pngs";
 
 export const demoProductsData = [
   {
-    image: productsDemoImg1,
+    image:
+      "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647006558/Company-website/productsDemoImg1_n7ctjc.png",
     title: "Ndu",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut diam volutpat nibh nulla donec rutrum morbi. Sapien pulvinar eget",
     link: "https://www.google.com",
+    size: { width: 862, height: 752 },
   },
   {
-    image: productsDemoImg2,
+    image:
+      "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1647006557/Company-website/productsDemoImg2_w4sblz.png",
     title: "Beima",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut diam volutpat nibh nulla donec rutrum morbi. Sapien pulvinar eget",
     link: "https://www.google.com",
+    size: { width: 868, height: 752 },
   },
 ];
 
