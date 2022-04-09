09346d6e6f6225fa153f6ea053aeb70db0207561
davien21 <chidiebereekennia@gmail.com>
2022-04-09 23:23:24 +0100

    --refactor: adjust text on testimonials

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -32,8 +32,8 @@ export const testimonials: Testimonial = [
   {
     name: "Mr. Tony Emeka",
     role: "CEO, CryptoTvPlus.",
-    title: "Part of the Sponsors of 2021 Blockchain Developer's Conf.",
-    desc: "We were part of the sponsors for the last Blockchain Developers Conference that was organised by Blockchain Hub Africa, and were honestly impressed with the team, and what they are building.",
+    title: "Sponsor Blockchain Developers Conference 2021",
+    desc: "It was a pleasure to be part of the sponsors of the blockchain developers Conference 2021.",
     headshot: TonyCryptoTVSrc,
   },
 ];
