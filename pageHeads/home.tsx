83062c1a8d73207bac1aff45e26cb12e4b249aa3
davien21 <chidiebereekennia@gmail.com>
2022-04-27 08:16:18 +0100

    --refactor: minor seo adjustments

--- pageHeads/home.tsx
+++ pageHeads/home.tsx
@@ -5,11 +5,11 @@ const HomePageHead = () => {
     <>
       <NextSeo
         title="Blockchain Hub Africa | Home"
-        description="Blockchain Hub Africa was startup established in 2021 to drive Blockchain and Web3 adoption by cultivating blockchain talent and solutions in Africa."
+        description="Blockchain Hub Africa is a startup established in 2021 to drive Blockchain and Web3 adoption by cultivating blockchain talent and solutions in Africa."
         openGraph={{
           title: "Blockchain Hub Africa | Home",
           description:
-            "Blockchain Hub Africa was startup established in 2021 to drive Blockchain and Web3 adoption by cultivating blockchain talent and solutions in Africa.",
+            "Blockchain Hub Africa is a startup established in 2021 to drive Blockchain and Web3 adoption by cultivating blockchain talent and solutions in Africa.",
           images: [
             {
               url: "https://res.cloudinary.com/blockchainhub-africa/image/upload/v1650293106/Company-website/bcha_logo_dark_pzqxcl.jpg",
