82cd54261078f15616fa2003108ef4abbcc236eb
davien21 <chidiebereekennia@gmail.com>
2022-11-09 21:42:16 +0100

    --feat: integrate registrations api

--- pages/events/bdc-2022/index.tsx
+++ pages/events/bdc-2022/index.tsx
@@ -18,10 +18,17 @@ import { CreateEventRegistrationAPI } from "../../../services/registrationServic
 import { apiErrorMessage } from "../../../utils/handleAPIErrors";
 import { toast } from "react-toastify";
 import action from "services/actionService";
-import {
-  paymentFailModalContent,
-  registrationSuccessModalContent,
-} from "./meta";
+
+export const registrationSuccessModalContent = {
+  title: "Registration Successful",
+  content:
+    "Congratulations, your registration was successful and details have been sent to your email.",
+};
+export const paymentFailModalContent = {
+  title: "Payment Unsuccessful",
+  content:
+    "Looks like you did not complete the payment, that's okay, we will be here if you change your mind.",
+};
 
 type ValuesType = {
   name: string;
